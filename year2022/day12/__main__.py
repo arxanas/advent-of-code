1d2c481d094cb0faa4d1f63eb8d1d4b87122e01d
Waleed Khan <me@waleedkhan.name>
2022-12-11 21:57:27 -0800

    Refactor year 2022 day 12

--- year2022/day12/__main__.py
+++ year2022/day12/__main__.py
@@ -32,12 +32,11 @@ def value_to_int(value: str) -> int:
         return ord(value)
 
 
-def part1(grid: Input) -> int:
-    start_coord = next(coord for (coord, value) in grid.iter_cells() if value == "S")
-    end_coord = next(coord for (coord, value) in grid.iter_cells() if value == "E")
-
-    best_lengths: dict[utils.Coord, int] = {start_coord: 0}
-    queue = collections.deque([start_coord])
+def shortest_path(
+    grid: Input, start_coords: set[utils.Coord], end_coord: utils.Coord
+) -> int:
+    best_lengths: dict[utils.Coord, int] = {coord: 0 for coord in start_coords}
+    queue = collections.deque(start_coords)
     while queue:
         current_coord = queue.popleft()
         if current_coord == end_coord:
@@ -58,6 +57,12 @@ def part1(grid: Input) -> int:
                 best_lengths[neighbor] = new_length
                 queue.append(neighbor)
     raise ValueError("No path found")
+
+
+def part1(grid: Input) -> int:
+    start_coord = next(coord for (coord, value) in grid.iter_cells() if value == "S")
+    end_coord = next(coord for (coord, value) in grid.iter_cells() if value == "E")
+    return shortest_path(grid, {start_coord}, end_coord)
 
 
 def test_part1() -> None:
@@ -65,37 +70,13 @@ def test_part1() -> None:
 
 
 def part2(grid: Input) -> int:
-    start_coords = [
+    start_coords = {
         coord
         for (coord, value) in grid.iter_cells()
         if value_to_int(value) == value_to_int("a")
-    ]
+    }
     end_coord = next(coord for (coord, value) in grid.iter_cells() if value == "E")
-
-    best_lengths: dict[utils.Coord, int] = {coord: 0 for coord in start_coords}
-    queue = collections.deque(start_coords)
-    while queue:
-        current_coord = queue.popleft()
-        if current_coord == end_coord:
-            # FIXME: Just got lucky here? Shouldn't we exhaust all paths to make
-            # sure there's no better path to the end?
-            return best_lengths[current_coord]
-
-        current_value = grid[current_coord]
-        neighbors = [
-            coord
-            for delta in utils.Deltas2d.CARDINAL
-            if (coord := current_coord + delta) in grid
-            if value_to_int(grid[coord]) - value_to_int(current_value) <= 1
-        ]
-
-        current_length = best_lengths[current_coord]
-        for neighbor in neighbors:
-            new_length = current_length + 1
-            if new_length < best_lengths.get(neighbor, float("inf")):
-                best_lengths[neighbor] = new_length
-                queue.append(neighbor)
-    raise ValueError("No path found")
+    return shortest_path(grid, start_coords, end_coord)
 
 
 def test_part2() -> None:
