36089bc01eb96c3edc87075b0e430154a8b99810
Waleed Khan <me@waleedkhan.name>
2024-11-30 19:14:33 -0800

    build: set up for year 2024

---
+++ year2024/template/__init__.py
@@ -0,0 +1,47 @@
+import logging
+import math
+import os
+import re
+import sys
+from collections import Counter, defaultdict, deque
+from dataclasses import dataclass, replace
+from functools import *
+from itertools import *
+
+import z3  # type: ignore[import]
+
+from .. import utils as u
+
+TEST_INPUT1 = r"""
+TODO
+"""
+
+TEST_INPUT2 = TEST_INPUT1
+
+PART_1_ANSWER = 0
+PART_2_ANSWER = 0
+
+
+def test_part1() -> None:
+    assert Solution.parse_input(TEST_INPUT1).part1() == PART_1_ANSWER
+
+
+def test_part2() -> None:
+    assert Solution.parse_input(TEST_INPUT2).part2() == PART_2_ANSWER
+
+
+@dataclass
+class Solution(u.Solution):
+    input: str
+
+    @classmethod
+    def parse_input(cls, input: str) -> "Solution":
+        return cls(input=input.strip())
+
+    def part1(self) -> int:
+        result = 0
+        return result
+
+    def part2(self) -> int:
+        result = 0
+        return result
